93350e88d8724d99e5b727008f780a55b6b421a8
Adeyossy <adeyossy1@gmail.com>
2021-11-25 03:34:15 +0100

    First dreampod website commit

---
+++ scripts/index.js
@@ -0,0 +1,98 @@
+const firstSection = document.querySelector("#first-section-carousel");
+const secondSection = document.querySelector("#second-section-carousel");
+const thirdSection = document.querySelector("#third-section-carousel");
+const fourthSection = document.querySelector("#fourth-section-carousel");
+const fifthSection = document.querySelector("#fifth-section-carousel");
+
+let isOpen = false;
+
+const orderButtons = document.getElementsByClassName("dreampod-category-description-contact");
+const dialogWindow = document.querySelector(".dreampod-category-dialog");
+const dialogWindows = document.getElementsByClassName("dreampod-category-dialog");
+// const isOpenArray = new Array(orderButtons.length).fill(false, 0, orderButtons.length);
+// dialogWindow.style.animation = "900ms ease-in 1 normal fadeUp;";
+
+const dialogWindowSubContainer = document.querySelector(".dreampod-category-dialog-subcontainer");
+const dialogWindowSubContainers = document.getElementsByClassName("dreampod-category-dialog-subcontainer");
+// dialogWindowSubContainer.style.animation = "900ms ease-in 1 normal slideUp";
+
+const closeDialogButton = document.querySelector(".dreampod-category-dialog-close");
+const closeDialogButtons = document.getElementsByClassName("dreampod-category-dialog-close");
+
+// const navContact = document.querySelector("a.dreampod-category-description-contact");
+// navContact.addEventListener('click', (el, ev) => {
+//   console.log("called");
+//   ev.preventDefault();
+// });
+
+const sections = [firstSection, secondSection, thirdSection, fourthSection, fifthSection];
+let counter = 0;
+
+setInterval(() => {
+  let length = sections.length;
+  sections[counter++ % length].style.opacity = "0";
+  if (counter / 5 >= 1 && counter % 5 == 0) {
+    for (let i = 0; i < length; i++) {
+      sections[i].style.opacity = "1";
+    }
+  }
+}, 6000);
+
+//change animation direction based on if the dialog window is open
+function changeAnimation(element, animationName) {
+  // element.style.animation = `900ms ease-out 1 reverse ${animationName}`;
+  element.style.animation = isOpen ? `300ms ease-out 1 reverse ${animationName}`
+    : `300ms ease-in 1 normal ${animationName}`;
+}
+
+//cleear the animation when it's ended or canceled
+function onAnimationFinished(event, element) {
+  element.style.animation = "";
+}
+
+//listen for clicks on all orderButtons
+for (let i = 0; i < orderButtons.length; i++) {
+  let orderButton = orderButtons.item(i);
+  orderButton.addEventListener('click', () => {
+    //display the dialog window
+    dialogWindow.style.display = "flex";
+
+    //pad for the scrollbar when overflow is set to hidden
+    if (window.matchMedia('(min-width: 1366px)').matches) document.body.style.paddingRight = '16px';
+    document.body.style.overflow = "hidden";
+    changeAnimation(dialogWindow, 'fadeUp');
+    changeAnimation(dialogWindowSubContainer, 'slideUp');
+    isOpen = true;
+  });
+
+  // let closeDialogButton = closeDialogButtons.item(i);
+  //close dialog window and animate as appropriate
+  closeDialogButton.addEventListener('click', () => {
+    changeAnimation(dialogWindow, 'fadeUp');
+    changeAnimation(dialogWindowSubContainer, 'slideUp');
+    setTimeout(() => {
+      document.body.style.overflow = "auto";
+      if (window.matchMedia('(min-width: 1366px)')) document.body.style.paddingRight = "initial";
+      dialogWindow.style.display = "none";
+      isOpen = false;
+    }, 300);
+  });
+
+  // let dialogWindow = dialogWindows.item(i);
+  dialogWindow.addEventListener('animationend', (event) => {
+    onAnimationFinished(event, dialogWindow);
+  });
+
+  dialogWindow.addEventListener('animationcancel', (event) => {
+    onAnimationFinished(event, dialogWindow);
+  });
+
+  // let dialogWindowSubContainer = dialogWindowSubContainers.item(i);
+  dialogWindowSubContainer.addEventListener('animationend', () => {
+    dialogWindowSubContainer.style.animation = "";
+  });
+
+  dialogWindowSubContainer.addEventListener('animationcancel', (event) => {
+    onAnimationFinished(event, dialogWindowSubContainer);
+  });
+}
